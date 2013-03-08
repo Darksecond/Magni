d0001b00b3a297a85c6fac50bc46d29b44a60cf0
Tim Peters <mail@darksecond.nl>
2013-03-08 10:38:15 +0100

    first pass of documentation

--- src/Behavior.h
+++ src/Behavior.h
@@ -6,13 +6,27 @@ namespace Ymir
 {
     class Entity;
     
+    /**
+     * A behavior works with components from one entity (and possibly it's parent).
+     * The bulk of the game logic is contained within behaviors.
+     */
     class Behavior
     {
     protected:
+        /**
+         * The entity this behavior belongs to.
+         */
         Entity* entity;
     public:
+        /**
+         * This is called one a tick, delta is the time between calls in seconds.
+         */
         virtual void update(double delta) = 0;
         
+        /**
+         * This is called when a message is received. 
+         * This is mostly used for collisions and triggers right now.
+         */
         virtual void receive(message_type_t type, const Entity& trigger)
         {
         }
--- src/Component.h
+++ src/Component.h
@@ -2,6 +2,9 @@
 
 namespace Ymir
 {
+    /**
+     * a basecomponent, you can use this to make maps which contain multiple different components
+     */
     class BaseComponent
     {
     public:
@@ -10,12 +13,24 @@ namespace Ymir
         static Type type_counter;
     };
 
+    /**
+     * A component, this is used to store data.
+     * You can interact with the data with behaviors or engines.
+     * uses http://en.wikipedia.org/wiki/Curiously_recurring_template_pattern
+     */
     template<typename Derived>
     class Component : public BaseComponent
     {
     public:
+        /**
+         * Parent of this component.
+         * This is set automatically by entity the component is contained in.
+         */
         Derived* parent;
 
+        /**
+         * This components' type. you can use this to build lists or maps of components.
+         */
         static const Type type()
         {
             static Type _type = type_counter++;
