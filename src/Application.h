e8a284690e3c0240202aad49c6c6dd982b3de11f
medragon <maartenezendam@gmail.com>
2013-03-26 13:56:49 +0100

    Refactor main classes
    Took code from main class and put in Application class

---
+++ src/Application.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GL/GLFW.h>
+#include <GLM/glm.hpp>
+
+#include "DirectoryManifest.h"
+#include "ResourceManager.h"
+#include "Texture.h"
+#include "Program.h"
+#include "ProgramResourceLoader.h"
+#include "Mesh.h"
+#include "Audio/Buffer.h"
+#include "Cubemap.h"
+#include "CubemapResourceLoader.h"
+#include "Gameplay.h"
+#include "TileMap.h"
+#include "EngineManager.h"
+#include "Renderer/RenderEngine.h"
+#include "BehaviorEngine.h"
+#include "CollisionEngine.h"
+#include "AudioEngine.h"
+
+namespace Ymir
+{
+    class Application {
+        private:
+            std::shared_ptr<DirectoryManifest> manifest;
+            ResourceManager<Texture> textureManager;
+            ResourceManager<Program, ProgramResourceLoader> programManager;
+            ResourceManager<Mesh> meshManager;
+            ResourceManager<Audio::Buffer> audioBufferManager;
+            ResourceManager<Cubemap, CubemapResourceLoader> cubemapManager;
+
+            Gameplay * gameplay;
+            EngineManager * engines;
+            RenderEngine * renderEngine;
+
+            glm::vec2 SCREEN_SIZE{800, 600};
+            double lastTime;
+
+        public:
+            Application();
+
+            void createManagers(std::string resourceDirectory);
+            void createEngines();
+            void buildGame();
+            void runGame();
+    };
+};
