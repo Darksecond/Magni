f8368843b46cf8eda561182602a728117c507575
Peter Heinen <peter.heinen@hotmail.com>
2013-04-02 09:51:45 +0200

    start of 2 player stuff

--- src/OwnerComponent.h
+++ src/OwnerComponent.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "Component.h"
+#include "Program.h"
+
+#include <GLM/glm.hpp>
+
+namespace Ymir
+{
+    class OwnerComponent : public Component<OwnerComponent>
+    {
+
+    public:
+        OwnerComponent(OwnerComponent&) = delete;
+    };
+};
