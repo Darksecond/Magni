3895ff7f1faebbd62e850127abf7fc64c9201176
Tim Peters <mail@darksecond.nl>
2013-05-06 16:27:49 +0200

    added array functionality

--- src/NetworkPacket.h
+++ src/NetworkPacket.h
@@ -1,11 +1,12 @@
 #pragma once
 
-#include "Networkvalue.h"
+#include "NetworkValue.h"
 
 #include <vector>
 #include <memory>
 #include <stdexcept>
 #include <cstring>
+#include <type_traits>
 
 class NetworkPacket
 {
@@ -13,21 +14,21 @@ public:
 	NetworkPacket() : _size(0), _count(0), _array(nullptr), _values(), _dirty(true), _readonly(false)
 	{
 	}
-
+    
 	explicit NetworkPacket(const uint8_t* array, size_t size) : _size(size), _count(0), _array(new uint8_t[size]), _values(), _dirty(false), _readonly(true)
 	{
 		memcpy(_array, array, size);
-
+        
 		build_count_from_array();
 	}
-
+    
 	~NetworkPacket()
 	{
 		delete _array;
 	}
-
+    
 	template <typename T>
-	void set(int index, T value)
+	void set(int index, const T value)
 	{
 		if(_readonly) throw std::runtime_error("this object is in read-only mode");
 		if(index >= _values.size())
@@ -35,11 +36,37 @@ public:
 			_values.resize(index+1);
 			_count = index + 1;
 		}
-
+        
 		_values.at(index) = std::move(std::unique_ptr<NetworkValue<T>>(new NetworkValue<T>(value)));
 		_dirty = true;
 	}
-
+    
+	/**
+	 * Use this to set arrays which have a build in known length, like string literals.
+	 */
+	template <typename T, size_t N>
+	void set_array(int index, const T (&value)[N])
+	{
+		set_array(index, value, N);
+	}
+    
+	/**
+	 * Use this to set arrays of which the compiler does not know the length.
+	 */
+	template <typename T>
+	void set_array(const int index, const T* value, const size_t N)
+	{
+		if(_readonly) throw std::runtime_error("this object is in read-only mode");
+		if(index >= _values.size())
+		{
+			_values.resize(index+1);
+			_count = index + 1;
+		}
+        
+		_values.at(index) = std::move(std::unique_ptr<NetworkArray<T>>(new NetworkArray<T>(value, N)));
+		_dirty = true;
+	}
+    
 	template <typename T>
 	void build_value_from_array(size_t index)
 	{
@@ -48,14 +75,14 @@ public:
 		{
 			size_t value_size = *reinterpret_cast<size_t*>(i);
 			i += sizeof(size_t);
-
+            
 			if(j == index)
 			{
 				if(index >= _values.size())
 				{
 					_values.resize(index+1);
 				}
-
+                
 				T value = *reinterpret_cast<T*>(i);
 				_values[index] = std::move(std::unique_ptr<NetworkValue<T>>(new NetworkValue<T>(value)));
 			}
@@ -63,22 +90,66 @@ public:
 			++j;
 		}
 	}
-
+    
+	template <typename T>
+	void build_value_array_from_array(size_t index)
+	{
+		int j = 0;
+		for(uint8_t* i = _array + sizeof(size_t); i < _array + _size;)
+		{
+			size_t value_size = *reinterpret_cast<size_t*>(i);
+			i += sizeof(size_t);
+            
+			if(j == index)
+			{
+				if(index >= _values.size())
+				{
+					_values.resize(index+1);
+				}
+                
+				T* value = reinterpret_cast<T*>(i);
+				_values[index] = std::move(std::unique_ptr<NetworkArray<T>>(new NetworkArray<T>(value, value_size/sizeof(T))));
+			}
+			i += value_size;
+			++j;
+		}
+	}
+    
 	template <typename T>
 	const T& get(int index)
 	{
 		if(index >= _count)
 			throw std::runtime_error("you are trying to access an invalid index");
-
+        
 		if(index >= _values.size() || _values[index] == nullptr)
 		{
 			build_value_from_array<T>(index);
 		}
-
+        
 		NetworkValue<T>* value = static_cast<NetworkValue<T>*>(_values[index].get());
 		return value->value();
 	}
-
+    
+	/**
+	 * Use this to access array elements.
+	 * The Type needs to be of a pointer type, like char* or int*
+	 * The Type must match the type used to set the element.
+	 */
+	template <typename T>
+	const typename std::remove_pointer<T>::type* get_array(int index)
+	{
+		if(index >= _count)
+			throw std::runtime_error("you are trying to access an invalid index");
+        
+		if(index >= _values.size() || _values[index] == nullptr)
+		{
+			build_value_array_from_array<typename std::remove_pointer<T>::type>(index);
+		}
+        
+		NetworkArray<typename std::remove_pointer<T>::type>* value = static_cast<NetworkArray<typename std::remove_pointer<T>::type>*>(_values[index].get());
+		return value->value();
+	}
+    
 	void build_array()
 	{
 		_size = sizeof(size_t); //header
@@ -88,13 +159,13 @@ public:
 				_size += ptr->size();
 		}
 		_size += _count * sizeof(size_t);
-
+        
 		delete _array;
-		_array = new uint8_t[_size]; 
-
+		_array = new uint8_t[_size];
+        
 		//build up _array
 		int i = 0;
-
+        
 		//header
 		
 		//number of values
@@ -120,7 +191,7 @@ public:
 				length.as_size_t = ptr->size();
 				memcpy(_array+i, length.as_char, sizeof(length));
 				i += sizeof(length);
-
+                
 				//body of value
 				memcpy(_array+i, ptr->to_char_array(), ptr->size());
 				i += ptr->size();
@@ -138,22 +209,22 @@ public:
 				i += sizeof(length);
 			}
 		}
-
+        
 		_dirty = false;
 	}
-
+    
 	const uint8_t* char_array()
 	{
 		if(_dirty) build_array();
 		return _array;
 	}
-
+    
 	size_t size()
 	{
 		if(_dirty) build_array();
 		return _size;
 	}
-
+    
 	size_t count()
 	{
 		return _count;
@@ -165,7 +236,7 @@ private:
 	std::vector<std::unique_ptr<BaseNetworkValue>> _values;
 	bool _dirty;
 	bool _readonly;
-
+    
 	void build_count_from_array()
 	{
 		_count = *reinterpret_cast<size_t*>(_array);
--- src/NetworkValue.h
+++ src/NetworkValue.h
@@ -19,14 +19,14 @@ public:
 	{
 		_data.object = value;
 	}
-
+    
 	const T& value() const { return _data.object; }
-
+    
 	virtual const uint8_t* to_char_array() const
 	{
 		return _data.char_array;
 	}
-
+    
 	virtual size_t size() const
 	{
 		return sizeof(T);
@@ -37,4 +37,37 @@ private:
 		T object;
 		uint8_t char_array[sizeof(T)];
 	} _data;
+};
+
+template <typename T>
+class NetworkArray : public BaseNetworkValue
+{
+public:
+	NetworkArray(const T* value, const size_t N) : _data(new T[N]), _count(N)
+	{
+		memcpy(_data, value, size());
+	}
+    
+	~NetworkArray()
+	{
+		delete _data;
+	}
+    
+	const T* value() const
+	{
+		return _data;
+	}
+    
+	virtual const uint8_t* to_char_array() const
+	{
+		return reinterpret_cast<const uint8_t*>(_data);
+	}
+    
+	virtual size_t size() const
+	{
+		return _count * sizeof(T);
+	}
+private:
+	T* _data;
+	const size_t _count;
 };
