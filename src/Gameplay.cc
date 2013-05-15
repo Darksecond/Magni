960771dfabc093e97d8eab65eeb377f38855b8f6
Peter Heinen <peter.heinen@hotmail.com>
2013-05-15 14:03:41 +0200

    Ip adres veranderd

--- src/Gameplay.cc
+++ src/Gameplay.cc
@@ -15,8 +15,8 @@ Gameplay::Gameplay(EngineManager& engineManager, CurrencyEngine& currencyEngine,
     client = new Client();
     client->gp = this;
 
-    //client->setIPAdress(192, 168, 0, 1);
-    client->setIPAdress(127, 0, 0, 1);
+    client->setIPAdress(192, 168, 0, 2);
+    //client->setIPAdress(127, 0, 0, 1);
 
     playernumber = 1;
 }
